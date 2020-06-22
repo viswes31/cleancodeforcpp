46785417405408098ee487cb638ac68a50e617e1
Sudeep Prasad <sudeep.rp@gmail.com>
2020-06-22 16:16:09 +0530

    Resolve gcc const warning

--- hello.cpp
+++ hello.cpp
@@ -4,8 +4,8 @@ private:
     int y;
 public:
     Point() { this->x = 0; this->y=0; }
-    const inline int getX() { return this->x; }
-    const inline int getY() { return this->y; }
+    inline int getX() { return this->x; }
+    inline int getY() { return this->y; }
     inline void setX(int x) { this->x = x; }
     inline void setY(int y) { this->y = y; }
 };
