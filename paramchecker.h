536e94472f543bd594e4fe6cbf0b1d19370e66a0
viswes31 <viswes31@gmail.com>
2020-07-21 16:11:52 +0530

    Added the definition for checkIfVitalOutOfRange fn

--- paramchecker.h
+++ paramchecker.h
@@ -1,5 +1,3 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate);
-bool checkIfBpmOutOfRange(float bpm);
-bool checkIfSpoOutOfRange(float spo2);
-bool checkIfRespRateOutOfRange(float respRate);
+bool checkIfVitalOutOfRange(float vital, float minRange, float maxRange);
