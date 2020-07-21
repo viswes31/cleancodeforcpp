aaced8f0cc3ec5cc185f16a64f93d9d5998a18e0
viswes31 <viswes31@gmail.com>
2020-07-21 16:15:26 +0530

    Corrected calling function for spo2 and resprate

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -5,5 +5,5 @@ bool checkIfVitalOutOfRange(float vital, float minRange, float maxRange)
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (checkIfVitalOutOfRange(bpm, 70, 150) ? false : (checkIfSpoOutOfRange(spo2, 80, 100) ? false : !checkIfRespRateOutOfRange(respRate, 30, 60)));
+  return (checkIfVitalOutOfRange(bpm, 70, 150) ? false : (checkIfVitalOutOfRange(spo2, 80, 100) ? false : !checkIfVitalOutOfRange(respRate, 30, 60)));
 }
