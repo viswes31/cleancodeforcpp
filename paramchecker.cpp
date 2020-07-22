886213b23d160d767378a568936ece9e2189e1c5
viswes31 <viswes31@gmail.com>
2020-07-22 15:20:49 +0530

    Corrected structe name in range definition

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -16,14 +16,14 @@ struct vitalsRange
 
 struct vitalsRange vitalRange[3];
 
-vitalsRange[BPM].minRange = 70;
-vitalsRange[BPM].maxRange = 70;
+vitalRange[BPM].minRange = 70;
+vitalRange[BPM].maxRange = 70;
 
-vitalsRange[SPO2].minRange = 70;
-vitalsRange[SPO2].maxRange = 70;
+vitalRange[SPO2].minRange = 70;
+vitalRange[SPO2].maxRange = 70;
 
-vitalsRange[RESPRATE].minRange = 70;
-vitalsRange[RESPRATE].maxRange = 70;
+vitalRange[RESPRATE].minRange = 70;
+vitalRange[RESPRATE].maxRange = 70;
 
 
 bool checkIfVitalOutOfRange(float vital, float minRange, float maxRange)
@@ -35,7 +35,7 @@ bool vitalsAreOk(std::vector<float> inputVitals) {
   bool areVitalsOk = true;
   for(int count=0; count < MAX_VITALS; ++count)
   {
-    if(checkIfVitalOutOfRange(inputVitals[count], vitalsRange[count].minRange, vitalsRange[count].maxRange) == false)
+    if(checkIfVitalOutOfRange(inputVitals[count], vitalRange[count].minRange, vitalRange[count].maxRange) == false)
     {
       areVitalsOk = false;
       break;
