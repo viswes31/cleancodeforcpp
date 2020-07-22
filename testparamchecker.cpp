0a29d54ba28785785aa5ec42bc59a5a0dd40a261
viswes31 <viswes31@gmail.com>
2020-07-22 15:06:25 +0530

    Modifies unit test for paramchecker with new funcs

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -2,11 +2,19 @@
 #include <gtest/gtest.h>
  
 TEST(VitalsTest, BPM) { 
+    std::vector<float> vitalList;
+    vitalList.push_back(100);
+    vitalList.push_back(100);
+    vitalList.push_back(50);
     ASSERT_EQ(true, vitalsAreOk(100, 100, 50));
 }
  
 TEST(VitalsTest, SPO2) {
-    ASSERT_EQ(false, vitalsAreOk(100, 40, 50));
+    std::vector<float> vitalList;
+    vitalList.push_back(100);
+    vitalList.push_back(40);
+    vitalList.push_back(50);
+    ASSERT_EQ(false, vitalsAreOk(vitalList));
 }
  
 int main(int argc, char **argv) {
